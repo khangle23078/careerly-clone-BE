930cbcfca47d97daf0c51b85b81ccf0ef9cb7fce
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-06-07 21:03:01 +0700

    feat: delete comment

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Comment from '../models/comment.model';
+import { deleteById, editById } from '../services/comment.service';
 
 export const createComment = async (req: Request, res: Response) => {
   try {
@@ -15,6 +16,42 @@ export const createComment = async (req: Request, res: Response) => {
       error: false,
       message: 'Create comment success',
     });
+  } catch (error: any) {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+export const editComment = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await editById(id);
+    res.status(200).json({
+      status: 200,
+      error: false,
+      message: 'Edit comment success',
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: error.message,
+    });
+  }
+};
+
+export const deleteComment = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json({
+      status: 200,
+      error: false,
+      message: 'Delete comment success',
+    });
+    await deleteById(id);
   } catch (error: any) {
     res.status(500).json({
       status: 500,
