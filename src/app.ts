fcf6150f07fd0ea9d96e37404f873869fc9ffb7b
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-06-18 01:50:15 +0700

    install cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDb } from './database/connect';
 import authRoute from './routes/auth.route';
 import postRoute from './routes/post.route';
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use(morgan('combined'));
 app.use('/api', authRoute);
 app.use('/api', postRoute);
